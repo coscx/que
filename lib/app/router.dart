700bf86e2e3220ab6ac446fbaa8c0758ff6484bb
coscx <710666260@qq.com>
2022-03-15 17:50:51 +0800

    am

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_geen/views/app/navigation/unit_navigation.dart';
-import 'package:flutter_geen/views/items/QrCode.dart';
 import 'package:flutter_geen/views/pages/about/about_me_page.dart';
 import 'package:flutter_geen/views/pages/about/about_app_page.dart';
 import 'package:flutter_geen/views/pages/about/person_center_page.dart';
-
 import 'package:flutter_geen/views/pages/about/version_info.dart';
 import 'package:flutter_geen/views/pages/category/category_detail.dart';
 import 'package:flutter_geen/views/pages/category/collect_page.dart';
@@ -12,11 +10,8 @@ import 'package:flutter_geen/views/pages/chat/chat_page.dart';
 import 'package:flutter_geen/views/pages/chat/conversation_list.dart';
 import 'package:flutter_geen/views/pages/chat/group_page.dart';
 import 'package:flutter_geen/views/pages/data/brower.dart';
-
 import 'package:flutter_geen/views/pages/gallery/gallery_page.dart';
 import 'package:flutter_geen/views/pages/index/new_amaps.dart';
-import 'package:flutter_geen/views/pages/index/amap_index.dart';
-import 'package:flutter_geen/views/pages/index/map.dart';
 import 'package:flutter_geen/views/pages/issues_point/issues_detail.dart';
 import 'package:flutter_geen/views/pages/issues_point/issues_point_page.dart';
 import 'package:flutter_geen/views/pages/login/login_page.dart';
@@ -33,7 +28,6 @@ import 'package:flutter_geen/views/pages/user/User.dart';
 import 'package:flutter_geen/views/pages/widget_detail/widget_detail_page.dart';
 import 'package:flutter_geen/views/pages/weixin/wx_search_page.dart';
 import 'package:flutter_geen/views/pages/setting/setting_page.dart';
-
 import 'utils/router_utils.dart';
 
 class UnitRouter {
@@ -47,7 +41,6 @@ class UnitRouter {
   static const String collect = 'CollectPage';
   static const String point = 'IssuesPointPage';
   static const String point_detail = 'IssuesDetailPage';
-
   static const String setting = 'SettingPage';
   static const String font_setting = 'FountSettingPage';
   static const String theme_color_setting = 'ThemeColorSettingPage';
@@ -55,10 +48,8 @@ class UnitRouter {
   static const String item_style_setting = 'ItemStyleSettingPage';
   static const String version_info = 'VersionInfo';
   static const String login = 'login';
-
   static const String category_show = 'CategoryShow';
   static const String issues_point = 'IssuesPointPage';
-
   static const String attr = 'AttrUnitPage';
   static const String bug = 'BugUnitPage';
   static const String galley = 'GalleryPage';
@@ -74,29 +65,33 @@ class UnitRouter {
   static const String brower = 'Brower';
   static const String recommended_card = 'RecommendedCard';
   static const String login_phone = 'LoginPhone';
-  static const String person_page ='PersonCenterPage';
-  static const String create_user_page ='CreateUserPage';
-  static const String select_page ='SelectPage';
-  static const String search_page_appoint ='SearchPageAppoint';
-  static const String baidu_map ='BaiduMap';
-  static const String user ='User';
-  static const String qr_view ='QrView';
+  static const String person_page = 'PersonCenterPage';
+  static const String create_user_page = 'CreateUserPage';
+  static const String select_page = 'SelectPage';
+  static const String search_page_appoint = 'SearchPageAppoint';
+  static const String baidu_map = 'BaiduMap';
+  static const String user = 'User';
+  static const String qr_view = 'QrView';
+
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       //根据名称跳转相应页面
       case widget_detail:
-        return Right2LeftRouter(
-            child: WidgetDetailPage(
-        ));
-
+        return Right2LeftRouter(child: WidgetDetailPage());
       case search:
         return Right2LeftRouter(child: SearchPage());
       case wx_search:
-        return Right2LeftRouter(child: WxSearchPage(selectItems: settings.arguments,));
+        return Right2LeftRouter(
+            child: WxSearchPage(
+          selectItems: settings.arguments,
+        ));
       case search_page_appoint:
         return Right2LeftRouter(child: SearchPageAppoint());
       case select_page:
-        return Right2LeftRouter(child: SelectPage(title: "ttt",));
+        return Right2LeftRouter(
+            child: SelectPage(
+          title: "ttt",
+        ));
       case collect:
         return Right2LeftRouter(child: CollectPage());
       case nav:
@@ -117,16 +112,12 @@ class UnitRouter {
         return Right2LeftRouter(child: VersionInfo());
       case issues_point:
         return Right2LeftRouter(child: IssuesPointPage());
-      // case qr_view:
-      //   return Right2LeftRouter(child: ScanView());
       case login:
         return Right2LeftRouter(child: LoginPage());
       case baidu_map:
         return Right2LeftRouter(child: AMaps());
-
       case galley:
         return Right2LeftRouter(child: GalleryPage());
-
       case about_app:
         return Right2LeftRouter(child: AboutAppPage());
       case about_me:
@@ -134,28 +125,33 @@ class UnitRouter {
       case to_chat:
         return Right2LeftRouter(child: AboutMePage());
       case to_chats:
-        return Right2LeftRouter(child: ChatsPage(model: settings.arguments,));
+        return Right2LeftRouter(
+            child: ChatsPage(
+          model: settings.arguments,
+        ));
       case to_group_chat:
-        return Right2LeftRouter(child: GroupChatPage(model: settings.arguments,));
+        return Right2LeftRouter(
+            child: GroupChatPage(
+          model: settings.arguments,
+        ));
       case point_detail:
         return Right2LeftRouter(child: IssuesDetailPage());
-      // case index_page:
-      //   return Right2LeftRouter(child: IndexPage());
       case login_phone:
         return Right2LeftRouter(child: LoginPhone());
-      // case recommended_card:
-      //   return Right2LeftRouter(child: RecommendedCard(onDelete: () {}, title: ' ', child: Container(),));
       case create_user_page:
         return Right2LeftRouter(child: CreateUserPage());
       case person_page:
         return Right2LeftRouter(child: MinePage());
-
-      // case time_line:
-      //   return Right2LeftRouter(child: TimelinePage(title: settings.arguments,));
       case brower:
-        return Right2LeftRouter(child: Brower(url: settings.arguments,));
+        return Right2LeftRouter(
+            child: Brower(
+          url: settings.arguments,
+        ));
       case chat_list:
-        return Right2LeftRouter(child: ImConversationListPage(memberId: settings.arguments,));
+        return Right2LeftRouter(
+            child: ImConversationListPage(
+          memberId: settings.arguments,
+        ));
       case category_show:
         return Right2LeftRouter(
             child: CategoryShow(
