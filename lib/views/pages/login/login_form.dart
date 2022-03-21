fedc27242443c8fa07078cb419d08a69ba919093
coscx <710666260@qq.com>
2022-03-21 15:22:26 +0800

    am

--- lib/views/pages/login/login_form.dart
+++ lib/views/pages/login/login_form.dart
@@ -16,12 +16,13 @@ import 'package:flutter_geen/views/dialogs/delete_category_dialog.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:fluwx/fluwx.dart' as fluwx;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class LoginFrom extends StatefulWidget {
   @override
   _LoginFromState createState() => _LoginFromState();
 }
 
-class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
+class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver {
   final _usernameController = TextEditingController(text: '');
   final _passwordController = TextEditingController(text: '');
 
@@ -31,73 +32,70 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
   @override
   void initState() {
     super.initState();
-    fluwx.weChatResponseEventHandler.distinct((a, b) => a == b).listen((res) async {
+    fluwx.weChatResponseEventHandler
+        .distinct((a, b) => a == b)
+        .listen((res) async {
       if (res is fluwx.WeChatAuthResponse) {
-          if(res.state =="wechat_sdk_demo_login"){
-            BlocProvider.of<LoginBloc>(context).add(
-              EventWxLogin(code: res.code),
-            );
-            if (!mounted) return;
-            setState(() {
-              _result = "state :${res.state} \n code:${res.code}";
-            });
-
-          }
-
+        if (res.state == "wechat_sdk_demo_login") {
+          BlocProvider.of<LoginBloc>(context).add(
+            EventWxLogin(code: res.code),
+          );
+          if (!mounted) return;
+          setState(() {
+            _result = "state :${res.state} \n code:${res.code}";
+          });
+        }
       }
     });
   }
+
   @override
   void dispose() {
     super.dispose();
     _result = null;
   }
+
   _showToast(BuildContext ctx, String msg, bool collected) {
     Toasts.toast(
       ctx,
       msg,
-      duration: Duration(milliseconds:  5000 ),
+      duration: Duration(milliseconds: 5000),
       action: collected
           ? SnackBarAction(
-          textColor: Colors.white,
-          label: '收藏夹管理',
-          onPressed: () => Scaffold.of(ctx).openEndDrawer())
+              textColor: Colors.white,
+              label: '收藏夹管理',
+              onPressed: () => Scaffold.of(ctx).openEndDrawer())
           : null,
     );
   }
+
   @override
   Widget build(BuildContext context) {
-    return
-
-      BlocListener<LoginBloc, LoginState>(
+    return BlocListener<LoginBloc, LoginState>(
         listener: (ctx, state) {
-            if (state is LoginFailed) {
-              Scaffold.of(context).showSnackBar(SnackBar(
+          if (state is LoginFailed) {
+            Scaffold.of(context).showSnackBar(SnackBar(
               content: Text('${state.reason}'),
               backgroundColor: Colors.red,
-          ));
-              BlocProvider.of<LoginBloc>(context).add(
-                EventLoginFailed(),
-              );
-        }
-                 if (state is LoginSuccess) {
-                   BlocProvider.of<LoginBloc>(context).add(
-                     EventLoginFailed(),
-                   );
+            ));
+            BlocProvider.of<LoginBloc>(context).add(
+              EventLoginFailed(),
+            );
+          }
+          if (state is LoginSuccess) {
+            BlocProvider.of<LoginBloc>(context).add(
+              EventLoginFailed(),
+            );
 
-                   BlocProvider.of<HomeBloc>(context).add(EventTabTap());
-                Navigator.of(context).pushReplacementNamed(UnitRouter.nav);
-            }
+            BlocProvider.of<HomeBloc>(context).add(EventTabTap());
+            Navigator.of(context).pushReplacementNamed(UnitRouter.nav);
+          }
         },
-        child: BlocBuilder<LoginBloc, LoginState>(
-              builder:_buildLoginByState
-          )
-          );
+        child: BlocBuilder<LoginBloc, LoginState>(builder: _buildLoginByState));
   }
 
-
-  Widget _buildLoginByState(BuildContext context,LoginState state) {
-    return  GestureDetector(
+  Widget _buildLoginByState(BuildContext context, LoginState state) {
+    return GestureDetector(
       behavior: HitTestBehavior.translucent,
       onTap: () {
         // 触摸收起键盘
@@ -105,119 +103,125 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
       },
       child: Stack(
         children: [
-        Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Text("ERP系统",style: TextStyle(fontSize: 40.sp),),
-          SizedBox(height: 15.h,),
-          Text("登录",style: TextStyle(color: Colors.grey),),
-          SizedBox(height:30.h,),
-          buildUsernameInput(),
-          Stack(
-            alignment: Alignment(0.8,0),
-            children: [
-              buildPasswordInput(),
-              FeedbackWidget(
-                  onPressed: ()=> setState(() => _showPwd=!_showPwd),
-                  child: Icon(_showPwd?TolyIcon.icon_show:TolyIcon.icon_hide)
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                "ERP系统",
+                style: TextStyle(fontSize: 40.sp),
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              Text(
+                "登录",
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(
+                height: 30.h,
+              ),
+              buildUsernameInput(),
+              Stack(
+                alignment: Alignment(0.8, 0),
+                children: [
+                  buildPasswordInput(),
+                  FeedbackWidget(
+                      onPressed: () => setState(() => _showPwd = !_showPwd),
+                      child: Icon(
+                          _showPwd ? TolyIcon.icon_show : TolyIcon.icon_hide))
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Checkbox(value: true, onChanged: (e) => {}),
+                  Text(
+                    "自动登录",
+                    style: TextStyle(color: Color(0xff444444), fontSize: 30.sp),
+                  ),
+                  Spacer(),
+                  GestureDetector(
+                      onTap: () {
+                        _register(context);
+                      },
+                      child: Text(
+                        "如何注册?",
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 25.sp,
+                            decoration: TextDecoration.underline),
+                      ))
+                ],
+              ),
+              _buildBtn(state),
+              buildOtherLogin(),
+              Container(
+                child: state is LoginLoading
+                    ? Container(
+                    padding: EdgeInsets.only(left: 0.w,  top:20.h),
+                    child: CircularProgressIndicator()
+                )
+                    : null,
               )
             ],
           ),
-          Row(
-            children: <Widget>[
-              Checkbox(value: true, onChanged: (e) => {}),
-              Text(
-                "自动登录",
-                style: TextStyle(color: Color(0xff444444), fontSize: 30.sp),
-              ),
-              Spacer(),
-              GestureDetector(
-                onTap: (){
-                  _register(context);
-                },
-                child:
-              Text(
-                "如何注册?",
-                style: TextStyle(
-                    color: Colors.blue,
-                    fontSize: 25.sp,
-                    decoration: TextDecoration.underline),
-              ))
-            ],
-          ),
-          _buildBtn(state),
-          buildOtherLogin(),
-          Container(
-            child: state is LoginLoading
-                ? CircularProgressIndicator()
-                : null,
-          )
-
         ],
-      ),
-
-
-        ],
-
-
       ),
     );
-
   }
 
   _register(BuildContext context) {
     showDialog(
         context: context,
         builder: (ctx) => Dialog(
-          elevation: 5,
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(10.w))),
-          child: Container(
-            width: 50.w,
-            child: DeleteCategoryDialog(
-              title: '注册功能暂未开放',
-              content: '请联系主管申请注册',
-              onSubmit: () {
-                //BlocProvider.of<HomeBloc>(context).add(EventResetCheckUser(photo,1));
-                Navigator.of(context).pop();
-              },
-            ),
-          ),
-        ));
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.w))),
+              child: Container(
+                width: 50.w,
+                child: DeleteCategoryDialog(
+                  title: '注册功能暂未开放',
+                  content: '请联系主管申请注册',
+                  onSubmit: () {
+                    //BlocProvider.of<HomeBloc>(context).add(EventResetCheckUser(photo,1));
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ),
+            ));
   }
-  void _doLogIn() {
 
-    print('---用户名:${_usernameController.text}------密码：${_passwordController.text}---');
-    if (_usernameController.text.isEmpty){
+  void _doLogIn() {
+    print(
+        '---用户名:${_usernameController.text}------密码：${_passwordController.text}---');
+    if (_usernameController.text.isEmpty) {
       return;
     }
-    if (_passwordController.text.isEmpty){
+    if (_passwordController.text.isEmpty) {
       return;
     }
-   BlocProvider.of<LoginBloc>(context).add(
-     EventLogin(
-         username: _usernameController.text,
-         password: _passwordController.text),
-   );
+    BlocProvider.of<LoginBloc>(context).add(
+      EventLogin(
+          username: _usernameController.text,
+          password: _passwordController.text),
+    );
   }
 
   Widget _buildBtn(LoginState state) => Container(
-    margin: EdgeInsets.only(top: 10.h, left: 10.w, right: 10.w,bottom: 0),
-    height: 80.h,
-    width: MediaQuery.of(context).size.width,
-    child:
-    RaisedButton(
-      elevation: 0,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(35.w))),
-      color: Colors.blue,
-      onPressed: state is LoginInital?  _doLogIn :null,
-      child: Text("登   录",
-          style: TextStyle(color: Colors.white, fontSize: 30.sp)),
-    ),
-  );
+        margin: EdgeInsets.only(top: 10.h, left: 10.w, right: 10.w, bottom: 0),
+        height: 80.h,
+        width: MediaQuery.of(context).size.width,
+        child: RaisedButton(
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(35.w))),
+          color: Colors.blue,
+          onPressed: state is LoginInital ? _doLogIn : null,
+          child: Text("登   录",
+              style: TextStyle(color: Colors.white, fontSize: 30.sp)),
+        ),
+      );
 
-  Widget buildUsernameInput(){
+  Widget buildUsernameInput() {
     return Column(
       children: [
         Container(
@@ -228,13 +232,11 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
             ),
             borderRadius: BorderRadius.circular(30.w),
           ),
-          margin:
-           EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+          margin: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
           child: Row(
             children: <Widget>[
               Padding(
-                padding:
-                EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
+                padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
                 child: Icon(
                   Icons.person_outline,
                   color: Colors.grey,
@@ -244,7 +246,7 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
                 height: 20.h,
                 width: 1.w,
                 color: Colors.grey.withOpacity(0.5),
-                margin:  EdgeInsets.only(left: 00.0, right: 10.w),
+                margin: EdgeInsets.only(left: 00.0, right: 10.w),
               ),
               Expanded(
                 child: TextField(
@@ -252,7 +254,7 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     hintText: '请输入用户名...',
-                    hintStyle: TextStyle(color: Colors.grey,fontSize: 30.sp),
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 30.sp),
                   ),
                 ),
               )
@@ -263,7 +265,7 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
     );
   }
 
-  Widget buildPasswordInput(){
+  Widget buildPasswordInput() {
     return Column(
       children: [
         Container(
@@ -274,13 +276,11 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
             ),
             borderRadius: BorderRadius.circular(30.w),
           ),
-          margin:
-           EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
+          margin: EdgeInsets.symmetric(vertical: 10.h, horizontal: 10.w),
           child: Row(
             children: <Widget>[
               Padding(
-                padding:
-                EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
+                padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
                 child: Icon(
                   Icons.lock_outline,
                   color: Colors.grey,
@@ -290,7 +290,7 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
                 height: 30.h,
                 width: 1.w,
                 color: Colors.grey.withOpacity(0.5),
-                margin:  EdgeInsets.only(left: 00.0, right: 10.w),
+                margin: EdgeInsets.only(left: 00.0, right: 10.w),
               ),
               Expanded(
                 child: TextField(
@@ -299,7 +299,7 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
                   decoration: InputDecoration(
                     border: InputBorder.none,
                     hintText: '请输入密码...',
-                    hintStyle: TextStyle(color: Colors.grey,fontSize: 30.sp),
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 30.sp),
                   ),
                 ),
               )
@@ -309,28 +309,38 @@ class _LoginFromState extends State<LoginFrom> with WidgetsBindingObserver{
       ],
     );
   }
-  Widget buildOtherLogin(){
+
+  Widget buildOtherLogin() {
     return Wrap(
       alignment: WrapAlignment.center,
       children: [
         Padding(
-          padding: const EdgeInsets.only(top:30.0),
+          padding: const EdgeInsets.only(top: 30.0),
           child: Row(
             children: [
-              Expanded(child: Divider(height: 20,)),
+              Expanded(
+                  child: Divider(
+                height: 20,
+              )),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text('',style: TextStyle(color: Colors.grey),),
+                child: Text(
+                  '',
+                  style: TextStyle(color: Colors.grey),
+                ),
               ),
-              Expanded(child: Divider(height: 20,)),
+              Expanded(
+                  child: Divider(
+                height: 20,
+              )),
             ],
           ),
         ),
         GestureDetector(
-          onTap: (){
+          onTap: () {
             fluwx
                 .sendWeChatAuth(
-                scope: "snsapi_userinfo", state: "wechat_sdk_demo_login")
+                    scope: "snsapi_userinfo", state: "wechat_sdk_demo_login")
                 .then((data) {});
           },
           child: SvgPicture.asset(
